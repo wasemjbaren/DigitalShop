b619b17ff3a0445af17655d544d4a4c9c99d5717
wasemjbaren <wasemjbaren@gmail.com>
2022-11-29 20:55:41 +0200

    new features

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 function Rating(props) {
-  const { rating, numReviews } = props;
+  const { rating, numReviews, caption } = props;
   return (
     <div className="rating">
       <span>
@@ -46,7 +46,6 @@ function Rating(props) {
           }
         />
       </span>
-
       <span>
         <i
           className={
@@ -58,7 +57,11 @@ function Rating(props) {
           }
         />
       </span>
-      <span> {numReviews} reviews</span>
+      {caption ? (
+        <span>{caption}</span>
+      ) : (
+        <span>{' ' + numReviews + ' reviews'}</span>
+      )}{' '}
     </div>
   );
 }
